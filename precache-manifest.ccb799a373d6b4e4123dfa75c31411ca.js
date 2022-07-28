033898b55cd0420cb3cbf52012228c362f575194
Kylejeong2 <Kylejeong21@gmail.com>
2022-07-27 21:43:42 -0700

    Updates

---
+++ precache-manifest.ccb799a373d6b4e4123dfa75c31411ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2755c99e1cd8c0718040",
+    "url": "/static/css/main.7bda0933.chunk.css"
+  },
+  {
+    "revision": "2755c99e1cd8c0718040",
+    "url": "/static/js/main.2755c99e.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "18836d79bd0a0930c4c1",
+    "url": "/static/js/2.18836d79.chunk.js"
+  },
+  {
+    "revision": "b29c9e157adc6ecf5862",
+    "url": "/static/js/runtime~main.b29c9e15.js"
+  },
+  {
+    "revision": "b1482bd851ae87b2a37e2826f5449af0",
+    "url": "/index.html"
+  }
+];
